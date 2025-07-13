8cb6267dd8d4881756ea8646da25e04fe09ad467
developer-sridhar <sridhar.chandrasekar28@gmail.com>
2025-07-13 12:08:06 +0530

    first commit

--- frontend/src/components/dashboard.jsx
+++ frontend/src/components/dashboard.jsx
@@ -1,10 +1,11 @@
+
 import React from 'react';
 
 const Dashboard = () => {
     return (
         <div>
             <h1>Dashboard</h1>
-            <p>Welcome to the dashboard!</p>
+            <p>Welcome to Programmer's Club</p>
         </div>
     );
 };
